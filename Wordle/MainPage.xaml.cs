2d9a5474d84abbf7b6e81aa87992db44840d24f7
QuantumJZ <sukiwhitex7@gmail.com>
2024-05-05 18:28:38 -0600

    Added Text Entry Functionality

--- Wordle/MainPage.xaml.cs
+++ Wordle/MainPage.xaml.cs
@@ -10,35 +10,60 @@
         public MainPage()
         {
             InitializeComponent();
-            //this.TextEntry.Focus();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
 
         // Border with letter color: #afafaf
 
         private void OnTextChanged(object sender, EventArgs e)
         {
-            if(TextEntry.Text[TextEntry.Text.Length - 1].ToString() == "\n")
             if (GameActive)
             {
-                if(x < 5)
+                // Delete button clicked
+                if(x > TextEntry.Text.Length)
                 {
+                    if (x != 0)
+                    {
+                        x--;
+                    }
                     Label currBox = (Label)this.FindByName("Box" + y + x);
-                    currBox.Text = TextEntry.Text[TextEntry.Text.Length - 1].ToString().ToUpper();
-                    x++;
+                    currBox.Text = "";
+                    Border currBorder = (Border)this.FindByName("Border" + y + x);
+                    currBorder.Stroke = Color.FromArgb("#59595a");
+                    TextEntry.Text = TextEntry.Text.Substring(0, x);
+                }
+                else
+                {
+                    if (x < 5)
+                    {
+                        Label currBox = (Label)this.FindByName("Box" + y + x);
+                        currBox.Text = TextEntry.Text[TextEntry.Text.Length - 1].ToString().ToUpper();
+                        Border currBorder = (Border)this.FindByName("Border" + y + x);
+                        currBorder.Stroke = Color.FromArgb("#afafaf");
+                        x++;
+                    }
+                    else
+                    {
+                        TextEntry.Text = TextEntry.Text.Substring(0, 5);
+                    }
                 }
             }
+        }
+
+        private void OnEnter(object sender, EventArgs e)
+        {
+
         }
     }
 
