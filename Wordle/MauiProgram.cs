90431518b89bd318b4570296bc831e5fa4ec1485
QuantumJZ <sukiwhitex7@gmail.com>
2024-05-09 14:07:00 -0600

    Maximized Screen by Default

--- Wordle/MauiProgram.cs
+++ Wordle/MauiProgram.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
 using Syncfusion.Maui.Core.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
 namespace Wordle
 {
@@ -14,6 +20,28 @@ namespace Wordle
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             }).UseMauiCommunityToolkit();
+            #if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(true, true);
+                                overlappedPresenter.Maximize();
+                                break;
+                        }
+                    });
+                });
+            });
+            #endif
             builder.ConfigureSyncfusionCore();
 #if DEBUG
             builder.Logging.AddDebug();
